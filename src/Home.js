4356176f6a38115e21e387dd67f2dfc978805ea6
Your Name <you@example.com>
2021-05-19 21:57:13 +0530

    new changes

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,12 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 import img2 from "./Images/logo.png";
 import "./Home.css";
-import { Form, Col, Button } from "react-bootstrap";
+import { Form, Col, Button, Accordion, Card } from "react-bootstrap";
 
 export const Home = () => (
   <div className="mainBody">
     <div className="covidlogo">
-      <img src={img2} alt="/" />
+      <img src={img2} alt="/" className="cowinlogo" />
     </div>
     <div className="mainDiv">
       <div className="name">
@@ -65,10 +65,51 @@ export const Home = () => (
         </div>
       </div>
       <div className="mybutton">
-        <Button variant="outline-success" size="lg">
+        <Button variant="outline-danger" size="lg">
           Get Notified
         </Button>{" "}
       </div>
+    </div>
+    <div className="FAQ">*General Questions/ FAQ</div>
+    <div className="acco">
+      <Accordion defaultActiveKey="0" className="pokemon">
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            How this site works?
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              Just enter either your pincode or state and district and this site
+              will give you instant notification whenever we detect an
+              availability. Note that to receive notification you have to keep
+              the site open
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="1">
+            How will I receive the notification?
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>
+              This site will make a bell like sound whenever an availability is
+              detected. We suggest you to also keep cowin site open in order to
+              immediately book the slot.
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="2">
+            Is my personal data collected?
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>
+              No we do not collect any personal data from the user. This site
+              remains 100% transparent. Any queries contact the developers
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
     </div>
   </div>
 );
