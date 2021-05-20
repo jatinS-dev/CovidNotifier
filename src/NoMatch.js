9a0d586be801c7b27d93833097e80590a3608c3d
Your Name <you@example.com>
2021-05-20 13:09:20 +0530

    Added heart

---
+++ src/NoMatch.js
@@ -0,0 +1,13 @@
+// import Image from 'react-bootstrap/Image';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+// import img2 from "./Images/logo.png";
+// import "./Vc.css";
+// import { Form, Col, Button, Accordion, Card } from "react-bootstrap";
+
+export const NoMatch = () => (
+  <div className="mainBody">
+    <h1>Yo</h1>
+  </div>
+);
---
+++ src/VaccineNotification.js
@@ -0,0 +1,18 @@
+// import Image from 'react-bootstrap/Image';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+// import img2 from "./Images/logo.png";
+import "./Vc.css";
+// import { Form, Col, Button, Accordion, Card } from "react-bootstrap";
+
+export const VaccineNotification = () => (
+  <div className="mainBody">
+    <div class="wrapper">
+      <div class="container">
+        <div class="heart"></div>
+      </div>
+    </div>
+    <h1>Finding Vaccine Slots for You. . .</h1>
+  </div>
+);
